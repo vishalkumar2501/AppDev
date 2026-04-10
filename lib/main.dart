2a8c6498d6427b02d7227a2b0e74ef7293d800c8
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-04-10 14:08:07 +0530

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
- runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
- const MyApp({super.key});
+  const MyApp({super.key});
 
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(title: Text("Tap Example")),
-       body: Center(
-         child: GestureDetector(
-           onTap: () {
-             print("Container tapped!");
-             
-           },
-           child: Container(
-             height: 100,
-             width: 100,
-             color: Colors.blue,
-             child: Center(child: Text("Tap Me")),
-           ),
-         ),
-       ),
-     ),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
 }
+
+class HomeScreen extends StatelessWidget {
+    
+}
+// import 'package:flutter/material.dart';
+
+// void main() {
+//  runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//  const MyApp({super.key});
+
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      home: Scaffold(
+//        appBar: AppBar(title: Text("Tap Example")),
+//        body: Center(
+//          child: GestureDetector(
+//            onTap: () {
+//              print("Container tapped!");
+             
+//            },
+//            child: Container(
+//              height: 100,
+//              width: 100,
+//              color: Colors.blue,
+//              child: Center(child: Text("Tap Me")),
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+// }
