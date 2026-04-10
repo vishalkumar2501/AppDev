aa74ef4384528a136f7c0d0320c2427e5ec3dbae
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-04-10 13:58:37 +0530

    added handlin taps

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+ runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+ const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My App',
-
-      // 🔵 Theme (AppBar blue + text white)
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-        ),
-      ),
-
-      home: const HomePage(),
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     home: Scaffold(
+       appBar: AppBar(title: Text("Tap Example")),
+       body: Center(
+         child: GestureDetector(
+           onTap: () {
+             print("Container tapped!");
+             
+           },
+           child: Container(
+             height: 100,
+             width: 100,
+             color: Colors.blue,
+             child: Center(child: Text("Tap Me")),
+           ),
+         ),
+       ),
+     ),
+   );
+ }
 }
